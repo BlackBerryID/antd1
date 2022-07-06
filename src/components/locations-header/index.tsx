25a96365a12b6a4f3d98383f8ded3e3252d50f18
BlackBerryID <iddf1994one@gmail.com>
2022-07-06 17:06:54 +0300

    feat: add button to locations header

--- src/components/locations-header/index.tsx
+++ src/components/locations-header/index.tsx
@@ -1,12 +1,19 @@
+import { Button } from 'antd';
+
 import './locations-header.scss';
 
 export const LocationsHeader = () => {
   return (
-    <div className="locations-header_info">
-      <div className="locations-header_info__title">Locations</div>
-      <div className="locations-header_info__text">
-        Create new users or update the existng users. You can then set their permissons here too.{' '}
+    <div className="locations-header">
+      <div className="locations-header_info">
+        <div className="locations-header_info__title">Locations</div>
+        <div className="locations-header_info__text">
+          Create new users or update the existng users. You can then set their permissons here too.{' '}
+        </div>
       </div>
+      <Button className="locations-header_button" type="primary">
+        Create Location
+      </Button>
     </div>
   );
 };
