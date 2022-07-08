46af96cb93963dd94d624e6d2aff27f355cdb944
BlackBerryID <iddf1994one@gmail.com>
2022-07-08 16:02:51 +0300

    feat: add btn on card-item

--- src/components/card-item/index.tsx
+++ src/components/card-item/index.tsx
@@ -1,13 +1,36 @@
-import { Card, Button, Avatar } from 'antd';
+import { Card, Button, Avatar, Dropdown, Menu } from 'antd';
+import { useState } from 'react';
 import { MENU_ICON_BLUE } from '../../app/constants';
 
 import './card-item.scss';
 
 export const CardItem = ({ cardData: { country, users } }: CardItemProps) => {
+  const [isDropdownActive, setIsDropdownActive] = useState<boolean>(false);
+
   const numberAvatarsToShow = users.length - 6 > 9 ? 6 : 7;
+  const menu = (
+    <Menu
+      items={[
+        {
+          label: 'Submit and continue',
+          key: '1',
+        },
+      ]}
+    />
+  );
   return (
     <Card className="card default">
       <h2 className="card_title">{country}</h2>
+      <Dropdown
+        className={`card_dropdown ${isDropdownActive ? 'active' : ''}`}
+        overlay={menu}
+        trigger={['click']}
+        onVisibleChange={(visible) => setIsDropdownActive(visible)}
+      >
+        <div className="card_dropdown__icon-wrapper">
+          <div className="card_dropdown__icon"></div>
+        </div>
+      </Dropdown>
       <div className="card_section">
         <div className="card_section_title">Holidays</div>
         <Button type="link">View Holidays</Button>
