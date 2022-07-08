52596a7919bdd8987630f20b60fede0840dcb052
BlackBerryID <iddf1994one@gmail.com>
2022-07-08 20:06:56 +0300

    feat: add close posibility to open-close submenu

--- src/components/submenu/index.tsx
+++ src/components/submenu/index.tsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
 import './submenu.scss';
 
+const CLOSE_STATE_WIDTH = '12px';
+const OPEN_STATE_WIDTH = '240px';
+
 export const SubMenu = () => {
+  const [active, setActive] = useState<boolean>(true);
+  const [submenuWidth, setSubmenuWidth] = useState(OPEN_STATE_WIDTH);
+
+  const toggleSubMenu = () => {
+    if (active) {
+      setActive(false);
+      setTimeout(() => {
+        setSubmenuWidth(CLOSE_STATE_WIDTH);
+      }, 500);
+    } else {
+      setSubmenuWidth(OPEN_STATE_WIDTH);
+      setTimeout(() => {
+        setActive(true);
+      }, 500);
+    }
+  };
+
   return (
-    <section className="submenu">
+    <section className={`submenu ${active ? 'active' : ''}`} style={{ width: submenuWidth }}>
       <h2 className="submenu_title">Settings</h2>
       <div className="submenu_item">
         <div className="submenu_item__icon general"></div>
@@ -20,8 +41,11 @@ export const SubMenu = () => {
           <div className="submenu_option__text">Locations</div>
         </div>
       </div>
-      <button className="submenu_arrow-back">
-        <div className="submenu_arrow-back__icon"></div>
+      <button className="submenu_arrow-back" onClick={() => toggleSubMenu()}>
+        <div
+          className="submenu_arrow-back__icon"
+          style={{ transform: `rotate(${active ? 0 : 180}deg)` }}
+        ></div>
       </button>
     </section>
   );
